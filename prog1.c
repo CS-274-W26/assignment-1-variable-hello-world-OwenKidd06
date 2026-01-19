c2f5d304b816f9c1cc2d565945f966fad06b7972
Owen Kidd <kiddo@oregonstate.edu>
2026-01-19 13:33:38 -0800

    Finished the code and ran it through valgrind and wall

--- prog1.c
+++ prog1.c
@@ -1,11 +1,24 @@
-#include <stdio.h> // Necessary for printf()
-
-// TODO Other includes
-
+#include <stdio.h>
+#include <time.h> 
+#include <stdio.h>  
+#include <stdlib.h>
 
 int main() {
-	// The main function is the entrypoint of the program. When the program
-	// is executed, it will automatically start here.
+	srand(time(NULL));
+
+	int myrand = rand()%11;
+
+	if (myrand <= 4) {
+		printf("Eat more beef, kick less cats \n");
+	}
+	else if (myrand >= 5 && myrand <= 9){
+		printf("FRODO LIVES \n");
+	}
+	else if (myrand == 10){
+		printf("Larn is the best roguelike \n");
+	}
 	
-	// TODO Complete the program
+	printf("The random number was %d\n", myrand);
+
+	return 0;
 }
